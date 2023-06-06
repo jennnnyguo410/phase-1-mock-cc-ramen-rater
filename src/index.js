468928e92b9b273257c0043240583b45a6b317de
Hao Guo <jennnnyguo@gmail.com>
2023-06-06 21:24:09 +1000

    Comment Update and Delete Button

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const showRating = document.getElementById('rating-display')
     const showComment = document.getElementById('comment-display')
     const form = document.getElementById('new-ramen')
-
+    const editForm = document.getElementById('edit-ramen')
+    const deleteBtn = document.getElementById('delete-button')
+    const editRating = document.getElementById('edit-rating')
+    const editComment = document.getElementById('edit-comment')
 
 
     // fetch the rscourse 
@@ -18,23 +21,43 @@ document.addEventListener('DOMContentLoaded', () => {
             //show first ramen's details 
             const firstRamen = ramens[0]
             menuDetail(firstRamen)
+            editFormValue(firstRamen)
             //show menu's ramens pics
             for (const ramen of ramens) {
                 menuDisplay(ramen)
             }
         })
+    let currentRamen = null
 
+    // display the menue's small img
     function menuDisplay(ramen) {
         const menuImg = document.createElement('img')
         menuImg.src = `${ramen.image}`
         menuImg.alt = `${ramen.name}`
         //add event listenser 
         menuImg.addEventListener('click', () => {
+            currentRamen = menuImg
+            editFormValue(ramen)
             menuDetail(ramen)
+
         })
         menu.appendChild(menuImg)
     }
 
+    deleteBtn.addEventListener('click', () => {
+        if (currentRamen) {
+            menu.removeChild(currentRamen)
+            currentRamen = null
+            clearDetail()
+        }
+    })
+
+    function editFormValue(ramen) {
+        editRating.placeholder = `${ramen.rating}`
+        editComment.placeholder = `${ramen.comment}`
+    }
+
+    //edit the big show ramen
     function menuDetail(ramen) {
         showImg.src = `${ramen.image}`
         showName.textContent = ramen.name
@@ -43,6 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
         showComment.textContent = ramen.comment
     }
 
+    //creat new ramen's menue
     form.addEventListener('submit', (e) => {
         e.preventDefault()
         let formData = {
@@ -56,5 +80,20 @@ document.addEventListener('DOMContentLoaded', () => {
         menuDisplay(formData)
     })
 
+    editForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        if (currentRamen) {
+            currentRamen.rating = editRating.value
+            currentRamen.comment = editComment.value
+            menuDetail(currentRamen)
+        }
+    })
 
+    function clearDetail() {
+        showImg.src = '';
+        showName.textContent = '';
+        showRestaurant.textContent = '';
+        showRating.textContent = '';
+        showComment.textContent = '';
+    }
 }); //END OF PAGE LOADED
